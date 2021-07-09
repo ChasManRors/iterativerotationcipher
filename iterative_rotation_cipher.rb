49fbf4acd6c2d578f4d10829e84897b9d5b92762
Charles Magid <cmmagid@gmail.com>
2021-07-09 13:44:13 -0400

    encode works on examples on to decode

--- iterative_rotation_cipher.rb
+++ iterative_rotation_cipher.rb
@@ -8,40 +8,31 @@ require 'pry'
 class IterativeRotationCipher
   def initialize; end
 
-
   def encode(number, string)
     result = string
     number.times do |_num|
       result = sub_encode(number, result)
-      puts result
     end
-    puts "#{number} #{result}"
     "#{number} #{result}"
   end
 
   def sub_encode(number, string)
     # Step 0 — index all spaces:
     indices = []
-    # string.each_char.with_index { |c, i| indices << i if /\s/ =~ c }
-    # string.each_char.with_index { |c, i| indices << i if c == ' ' }
-    string.each_char.with_index { |c, i| indices << [i, :space] if c == ' '; indices << [i, :newline] if c == "\n" }
-    binding.pry
-    
-    # Step 1 — remove all spaces:
+    string.each_char.with_index { |c, i| indices << i if c == ' ' }
+
+    # Step 1 remove all spaces:
     string2 = string.gsub(/ /, '')
-    
-    # Step 2 — shift the order of string characters to the right by 10:
+
+    # Step 2 shift the order of string characters to the right by 10:
     string3 = string2.split('').rotate(-number).join
-    
-    # Step 3 — place the spaces back in their original positions:
-    # indices.each_with_object(string3){ |index, string4| string4.insert(index, ' ') }
-    string4 = indices.each_with_object(string3) { |index, tmp| 
-      
-      # tmp.insert(index.first, "\n") if (index.last == :newline)
-    tmp.insert(index.first, " ") if (index.last == :space)
-    }
-    # Step 4 — shift the order of characters for each space-separated substring to the right by 10:
-    string4.split(' ').map { |substr| substr.split('').rotate(-number).join }.join(' ')
+
+    # Step 3 place the spaces back in their original positions:
+    string4 = string3.dup
+    indices.each_with_object(string4) { |index, a| a.insert(index, ' ') }
+
+    # Step 4 shift the order of characters for each space-separated substring to the right by 10:
+    string4.split(/ |\t|\r|\f/).map { |substr| substr.split('').rotate(-number).join }.join(' ')
   end
 
   def decode(string)
